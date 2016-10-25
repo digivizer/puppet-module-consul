258a7befd011223de9b214e63278fd19bc254af0
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2016-10-25 15:45:17 +1100

    add a version parameter and set default to Consul 0.7.0

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,40 +1,42 @@
+# installs and configures Consul agents (both client and server)
 define consul::agent(
-		$server         = false,
-		$dc             = "dc1",
-		$join           = undef,
-		$client_address = "127.0.0.1",
-		$advertise      = undef,
-		$expect         = undef,
+    $server         = false,
+    $dc             = 'dc1',
+    $join           = undef,
+    $client_address = '127.0.0.1',
+    $advertise      = undef,
+    $expect         = undef,
 ) {
-	include consul::install
+  include consul::install
 
-	file { "/var/local/consul/${name}":
-		ensure => directory,
-		mode   => 0700,
-		owner  => "consul",
-		group  => "consul",
-	}
+  $work_dir = "/var/local/consul/${name}"
 
-	if $server {
-		if $expect == undef {
-			fail "I don't know how many servers to expect"
-		}
+  file { $work_dir:
+    ensure => directory,
+    mode   => '0700',
+    owner  => 'consul',
+    group  => 'consul',
+  }
 
-		$server_opt = " -server -bootstrap-expect=$expect"
-	}
+  if $server {
+    if $expect == undef {
+      fail "I don't know how many servers to expect"
+    }
 
-	if $advertise {
-		$adv_opt = " -advertise ${advertise}"
-	}
+    $server_opt = " -server -bootstrap-expect=${expect}"
+  }
 
-	if $join {
-		if $join =~ /:.*:/ {
-			# Fuck you, Go, and your retro-90s style IPv6 address formats
-			$join_opt = " -retry-join='[${join}]:8301'"
-		} else {
-			$join_opt = " -retry-join=$join"
-		}
-	}
+  if $advertise {
+    $adv_opt = " -advertise ${advertise}"
+  }
+
+  if $join {
+    if $join =~ /:.*:/ {
+      $join_opt = " -retry-join='[${join}]:8301'"
+    } else {
+      $join_opt = " -retry-join=${join}"
+    }
+  }
 
   class { 'datadog_agent::integrations::consul':
     url               => 'http://localhost:8500',
@@ -42,19 +44,19 @@ define consul::agent(
     new_leader_checks => true,
   }
 
-  file { "/var/local/consul/${name}/default.json":
+  file { "${work_dir}/default.json":
     content => '{"statsd_addr": "127.0.0.1:8125"}',
-    mode   => 0700,
-    owner  => 'consul',
-    group  => 'consul',
+    mode    => '0700',
+    owner   => 'consul',
+    group   => 'consul',
   }
 
-	daemontools::service { "consul-${name}":
-		command => "/usr/local/bin/consul agent${server_opt}${join_opt}${adv_opt} -client=${client_address} -node=${name} -dc=${dc} -data-dir=/var/local/consul/${name} -pid-file=/var/local/consul/${name}.pid -config-file=/var/local/consul/${name}/default.json",
-		user    => "consul",
-		require => File["/var/local/consul/${name}", "/var/local/consul/${name}/default.json"],
-		environment => {
-			"GOMAXPROCS" => "2",
-		}
-	}
+  daemontools::service { "consul-${name}":
+    command     => "/usr/local/bin/consul agent${server_opt}${join_opt}${adv_opt} -client=${client_address} -node=${name} -dc=${dc} -data-dir=${work_dir} -pid-file=${work_dir}.pid -config-file=${work_dir}/default.json",
+    user        => 'consul',
+    require     => File[$work_dir, "${work_dir}/default.json"],
+    environment => {
+      'GOMAXPROCS' => '2',
+    }
+  }
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,28 +1,30 @@
-class consul::install {
-	file { "/var/local/consul":
-		ensure => directory,
-		mode   => 0730,
-		owner  => "root",
-		group  => "consul",
-	}
+# Install class for Consul
+class consul::install(
+    $version         = '0.7.0',
+) {
+  file { '/var/local/consul':
+    ensure => directory,
+    mode   => '0730',
+    owner  => 'root',
+    group  => 'consul',
+  }
 
-	group { "consul": ensure => present }
-	user { "consul": gid => "consul" }
+  group { 'consul': ensure => present }
+  user { 'consul': gid => 'consul' }
 
-	# Fuck I hate people who don't provide decent packages
-	file { "/usr/local/bin/install-consul":
-		ensure => file,
-		source => "puppet:///modules/consul/usr/local/bin/install-consul",
-		mode   => 0555,
-		owner  => "root",
-		group  => "root",
-	}
+  file { '/usr/local/bin/install-consul':
+    ensure => file,
+    source => 'puppet:///modules/consul/usr/local/bin/install-consul',
+    mode   => '0555',
+    owner  => 'root',
+    group  => 'root',
+  }
 
-	ensure_packages("unzip", { ensure => present })
+  ensure_packages('unzip', { ensure => present })
 
-	exec { "download and install consul":
-		command => "/usr/local/bin/install-consul",
-		creates => "/usr/local/bin/consul",
-		require => Package["unzip"],
-	}
+  exec { 'download and install consul':
+    command => "/usr/local/bin/install-consul ${version}",
+    creates => "/usr/local/bin/consul-${version}",
+    require => Package['unzip'],
+  }
 }
